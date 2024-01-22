3164edb18f4a7ead6132b66ecce20995bf3691da
tr33b <157428398+tr33b@users.noreply.github.com>
2024-01-22 15:26:56 -0800

    Update CPX-CPB-NeoPixel-1-LED-Blink.py

--- CPX-CPB-NeoPixel-1-LED-Blink.py
+++ CPX-CPB-NeoPixel-1-LED-Blink.py
@@ -20,7 +20,7 @@ import neopixel
 # Calls the circuit Python neopixel library, specifies the default board 
 # pins for the Neopixels, and the number of neopixels to access.  Returns a 
 # list 'pixels' of indexable neopixles
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 2)
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 9)
 
 while True:
     # indexes the first element of the 'pixels' list and points to the 
@@ -30,6 +30,34 @@ while True:
     pixels.fill((0, 0, 0))
     time.sleep(0.5)
     pixels[1]=(0,255, 0)
+    time.sleep(0.5)
+    pixels.fill((0, 0, 0))
+    time.sleep(0.5)
+    pixels[2]=(0, 0, 255)
+    time.sleep(0.5)
+    pixels.fill((0, 0, 0))
+    time.sleep(0.5)
+    pixels[3]=(0,255, 0)
+    time.sleep(0.5)
+    pixels.fill((0, 0, 0))
+    time.sleep(0.5)
+    pixels[4]=(255, 0, 0)
+    time.sleep(0.5)
+    pixels.fill((0, 0, 0))
+    time.sleep(0.5)
+    pixels[5]=(0,255, 0)
+    time.sleep(0.5)
+    pixels.fill((0, 0, 0))
+    time.sleep(0.5)
+    pixels[6]=(0, 0, 255)
+    time.sleep(0.5)
+    pixels.fill((0, 0, 0))
+    time.sleep(0.5)
+    pixels[7]=(10,20, 0)
+    time.sleep(0.5)
+    pixels.fill((0, 0, 0))
+    time.sleep(0.5)
+    pixels[8]=(0, 10, 20)
     time.sleep(0.5)
     pixels.fill((0, 0, 0))
     time.sleep(0.5)
