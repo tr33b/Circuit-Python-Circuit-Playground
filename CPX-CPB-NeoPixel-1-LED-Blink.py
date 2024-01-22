9f536b52a69f45a9f6e1725a57be89ebe8267af8
tr33b <157428398+tr33b@users.noreply.github.com>
2024-01-22 15:21:08 -0800

    Update CPX-CPB-NeoPixel-1-LED-Blink.py

--- CPX-CPB-NeoPixel-1-LED-Blink.py
+++ CPX-CPB-NeoPixel-1-LED-Blink.py
@@ -20,12 +20,16 @@ import neopixel
 # Calls the circuit Python neopixel library, specifies the default board 
 # pins for the Neopixels, and the number of neopixels to access.  Returns a 
 # list 'pixels' of indexable neopixles
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 2)
 
 while True:
     # indexes the first element of the 'pixels' list and points to the 
     # first (and only) Neopixel in the 'pixels' list
     pixels[0]=(255, 0, 0)
+    time.sleep(0.5)
+    pixels.fill((0, 0, 0))
+    time.sleep(0.5)
+    pixels[1]=(0,255, 0)
     time.sleep(0.5)
     pixels.fill((0, 0, 0))
     time.sleep(0.5)
